43b38a0edde4da6fce874acb5c8c39194adb7346
Janji03 <126324783+Janji03@users.noreply.github.com>
2023-03-04 13:12:54 +0100

    java

--- java/lezione11.java
+++ java/lezione11.java
@@ -1,3 +1,7 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class lezione11 {
     public static void main(String[] args){
         //Polimorfismo 
@@ -8,6 +12,34 @@ public class lezione11 {
         for (persona i : classe)
         {
             i.saluta(insegnante);
+        }
+        // Exception 
+        // il try prova una porzione di codice
+        // i catch gestiscono le diverse eccezioni
+        // finally esegue una porzione di codice alla fine a prescindere dal try
+        Scanner scanner = new Scanner(System.in);
+        try{
+        System.out.println("inserisci un numero\n");
+        int x = scanner.nextInt();
+        System.out.println("inserisci un numero\n");
+         int y = scanner.nextInt();
+        int r = x/y; // se metto y = 0 ottengo un exception 
+        System.out.println(r);
+        }catch(ArithmeticException e)
+        {
+            System.out.println("non puoi dividere per zero");
+        }
+        catch(InputMismatchException e)
+        {
+            System.out.println("non puoi dividere un numero per una stringa ");
+        }
+        catch(Exception e )
+        {
+            System.out.println("c'è stato un problema");
+        }
+        finally  
+        {
+            scanner.close();
         }
         
     }
